f67b739d41904b510759aa63117f752f18a5446f
Japsimar Soin <japsimarsoin2003@gmail.com>
2024-05-19 00:03:43 +0530

    todoServer solution using file updated

--- week-2/Week-2-Assignments/02-nodejs/todoServer.file.js
+++ week-2/Week-2-Assignments/02-nodejs/todoServer.file.js
@@ -24,98 +24,95 @@ app.get("/todos", (req, res) => {
 });
 
 app.get("/todos/:id", (req, res) => {
-    fs.readFile("todos.json", "utf8", (err, data) => {
-        if(err) throw err;
-        else{
-            const todos = JSON.parse(data);
-            const idx = getIdx(todos, parseInt(req.params.id));
-            if(idx === -1){
-                res.status(404).send();
-            }
-            else{
-                res.json(todos[idx]);
-            }
-        }
-    })
-})
+	fs.readFile("todos.json", "utf8", (err, data) => {
+		if (err) throw err;
+		else {
+			const todos = JSON.parse(data);
+			const idx = getIdx(todos, parseInt(req.params.id));
+			if (idx === -1) {
+				res.status(404).send();
+			} else {
+				res.json(todos[idx]);
+			}
+		}
+	});
+});
 
 app.post("/todos/:id", (req, res) => {
-    const todo = {
-        id: Math.floor(Math.random()*1000000),
-        title: req.body.title,
-        description: req.body.description
-    }
-    fs.readFile("todos.json", "utf8", (err, data) => {
-        if(err) throw err;
-        else{
-            const todos = JSON.parse(data);
-            todos.push(todo);
-            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
-                if(err) throw err;
-                else{
-                    res.status(201).json(todo);
-                }
-            })
-        }
-    })
-})
+	const todo = {
+		id: Math.floor(Math.random() * 1000000),
+		title: req.body.title,
+		description: req.body.description,
+	};
+	fs.readFile("todos.json", "utf8", (err, data) => {
+		if (err) throw err;
+		else {
+			const todos = JSON.parse(data);
+			todos.push(todo);
+			fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+				if (err) throw err;
+				else {
+					res.status(201).json(todo);
+				}
+			});
+		}
+	});
+});
 
 app.put("todos/:id", (req, res) => {
-    fs.readFile("todos.json", "utf8", (err, data) => {
-        const todos = JSON.parse(data);
-        const idx = getIdx(todos, parseInt(req.params.id));
-        if(idx === -1){
-            res.status(404).send();
-        }
-        else{
-            const todo = {
-                id: todos[idx].id,
-                title: req.body.title,
-                description: req.body.description
-            };
-            todos[idx] = todo;
-            fs.writeFile("todos.json", "utf8", (err) => {
-                if(err) throw err;
-                else{
-                    res.status(200).json(todo);
-                }
-            })
-        }
-    })
-})
+	fs.readFile("todos.json", "utf8", (err, data) => {
+		const todos = JSON.parse(data);
+		const idx = getIdx(todos, parseInt(req.params.id));
+		if (idx === -1) {
+			res.status(404).send();
+		} else {
+			const todo = {
+				id: todos[idx].id,
+				title: req.body.title,
+				description: req.body.description,
+			};
+			todos[idx] = todo;
+			fs.writeFile("todos.json", "utf8", (err) => {
+				if (err) throw err;
+				else {
+					res.status(200).json(todo);
+				}
+			});
+		}
+	});
+});
 
-function deleteIdx(arr, idx){
-    newTodos = [];
-    for(let i=0; i<arr.length; i++){
-        if(i !== idx){
-            newTodos.push(arr[i]);
-        }
-    }
-    return newTodos;
+function deleteIdx(arr, idx) {
+	newTodos = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (i !== idx) {
+			newTodos.push(arr[i]);
+		}
+	}
+	return newTodos;
 }
 
 app.delete("/todos/:id", (req, res) => {
-    fs.readFile("todos.json", "utf8", (err, data) => {
-        if(err) throw err;
-        const idx = getIdx(arr, id);
-        if(idx === -1){
-            res.status(404).send();
-        }
-        else{
-            todos = deleteIdx(todos, idx);
-            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
-                if(err) throw err;
-                else{
-                    res.status(200).send();
-                }
-            })
-        }
-    })
-})
+	fs.readFile("todos.json", "utf8", (err, data) => {
+		if (err) throw err;
+		const idx = getIdx(arr, id);
+		if (idx === -1) {
+			res.status(404).send();
+		} else {
+			todos = deleteIdx(todos, idx);
+			fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+				if (err) throw err;
+				else {
+					res.status(200).send();
+				}
+			});
+		}
+	});
+});
 
-app.use((req, res, next) =>{
-    res.status(404).send();
-})
+app.use((req, res, next) => {
+	res.status(404).send();
+});
 
 app.listen(3000);
 // module.exports = app;
